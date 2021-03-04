dbbf478bd104ce7045fad424cf2805ede89356ce
chansoo1280 <>
2021-03-04 17:47:40 +0900

    build

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -44,7 +44,9 @@ export default {
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+   
+  },
   router: { base: process.env.NODE_ENV === 'production'
   ? '/portfolio'
   : '/' },
